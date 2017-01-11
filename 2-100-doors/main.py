6997cf484a739c1739a513d51aedfdc49fc4946b
David Szilagyi <szilagyidav@gmail.com>
2017-01-11 18:43:39 +0100

    All tests are OK!

--- 2-100-doors/main.py
+++ 2-100-doors/main.py
@@ -8,7 +8,7 @@ from door import Door
 #   - count: integer - number of Door instances in the returned list (default=100)
 # Return: List of Door object instances with defined id values
 def generate_doors(count=100):
-    return []
+    return [Door(index + 1) for index in range(count)]
 
 
 # Does the main 100 logic. Goes through all the Door instances by the rules of 100 Doors
@@ -17,7 +17,10 @@ def generate_doors(count=100):
 #   - doors: list of Door objects
 # Return: list of toggled Door instances
 def toggle_doors(doors):
-    return []
+    for index in range(len(doors)):
+        for door in range(index, len(doors), index + 1):
+            doors[door].toggle()
+    return doors
 
 
 # Colects the ids of the open Doors form the given Door list
@@ -26,7 +29,7 @@ def toggle_doors(doors):
 #   - doors: list of (preferably toggled) Door objects
 # Return: list of open Door ids as strings
 def collect_open_doors(doors):
-    return []
+    return [str(doors[index].id) for index in range(len(doors)) if doors[index].is_open]
 
 
 # Formats a string list into a printable string
@@ -35,7 +38,7 @@ def collect_open_doors(doors):
 #   - open_door_list: list of strings
 # Return: string - comma separated values from the string list
 def format_open_door_names(open_door_ids):
-    return ""
+    return ", ".join(open_door_ids)
 
 
 # Main logic of the 100doors example script
